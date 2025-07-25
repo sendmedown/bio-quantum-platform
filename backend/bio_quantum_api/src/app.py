066bcd7c00161336a21a6a0e7174a6477f5f532f
Richard <sendmedown69@gmail.com>
2025-07-25 00:33:24 -0400

    Deleted main.py to avoid Render conflict

--- backend/bio_quantum_api/src/app.py
+++ backend/bio_quantum_api/src/app.py
@@ -164,6 +164,6 @@ if __name__ == '__main__':
     
     # Production-ready server configuration
     # Safe fallback for both production and development
-	socketio.run(app, host='0.0.0.0', port=port, debug=False, 	allow_unsafe_werkzeug=True)
+	socketio.run(app, host='0.0.0.0', port=5000, debug=False, 	allow_unsafe_werkzeug=True)
 
 
